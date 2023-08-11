79a1c312637c53a829e13a951a01ece53d149d1d
Manoj Rajure <rajuremanoj681@gmail.com>
2023-08-11 11:00:39 +0530

    commit

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_item.dart';
+
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,35 +11,63 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(
+  final List<GroceryItem> _groceryItem = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
+
+    if (newItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryItem.add(newItem);
+    });
+  }
+
+  void _removedItem(GroceryItem item) {
+    setState(() {
+      _groceryItem.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No item added yet!'),
+    );
+
+    if (_groceryItem.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItem.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removedItem(_groceryItem[index]);
+          },
+          key: ValueKey(_groceryItem[index].id),
+          child: ListTile(
+            title: Text(_groceryItem[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItem[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItem[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            groceryItems[index].quantity.toString(),
-          ),
-        ),
-      ),
+      body: content,
     );
   }
 }
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -13,14 +15,19 @@ class _NewItemState extends State<NewItem> {
   final _formKey = GlobalKey<FormState>();
   var _enteredName = '';
   var _enteredQuantity = 1;
-  var _selectedCategory = categories[Categories.vegetables];
+  var _selectedCategory = categories[Categories.vegetables]!;
 
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      print(_enteredName);
-      print(_enteredQuantity);
-      print(_selectedCategory);
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toString(),
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
     }
   }
 
@@ -103,7 +110,7 @@ class _NewItemState extends State<NewItem> {
                       ],
                       onChanged: (value) {
                         setState(() {
-                          _selectedCategory = value;
+                          _selectedCategory = value!;
                         });
                       },
                     ),
